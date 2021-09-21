99df99eee3123bde030662ff3f4d068be80e7c51
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-09-21 15:59:04 +0600

    👑Express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import { Falcon } from './client/Client';
+import express from 'express';
+
+const app = express();
+app.get('/', (req, res) => res.status(200).json({ msg: '🚀' }));
+app.listen(8080, () => {
+    console.log("Success")
+});
+
 import { env } from './client/env';
 new Falcon().start({
     token: env.token,
